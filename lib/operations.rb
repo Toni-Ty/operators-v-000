abcebe06b958ce29ba9b7a04d6f75be57094d714
Toni-Ty <antonique2479@yahoo.com>
2019-07-23 05:34:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,3 +10,59 @@ end
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
 end
+
+
+
+
+	def unsafe?(speed)
+		if speed < 40 || speed > 60
+			return true
+		else
+			return false
+		end
+end
+		def unsafe?(speed)
+			speed < 40 || speed > 60 ? "dangerous" : "safe" 
+		end
+unsafe?(100)
+
+
+def safe?(speed)
+  speed < 50 || speed > 30  ? true : false
+end
+safe?(80)
+
+
+def baby
+baby = 8		
+baby < 2 ? "baby" : "not a baby"  
+
+end
+
+baby
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
